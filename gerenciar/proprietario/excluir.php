cc470f5e0a0e9b3d94f77f6f9b43b2bd12169139
root <root@ubuntu.localdomain>
2020-06-11 10:07:24 -0300

    Desenvolvimento da rotina de gravar novo proprietário de caminhão.

---
+++ gerenciar/proprietario/excluir.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once '../../header.php';
+
+if (!isset($_SESSION['USER_ID']))
+{
+    header('Location: /login/index.php');
+}
+elseif (strcmp($_SERVER['REQUEST_METHOD'], 'POST') !== 0)
+{
+    header('Content-type: application/json');
+    echo json_encode('Método inválido.');
+}
+else
+{
+    $id = $_POST['id'];
+    $control = new \scr\control\MotoristaControl();
+
+    header('Content-type: application/json');
+    echo $control->excluir($id);
+}
---
+++ src/control/ProprietarioControl.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace scr\control;
+
+
+class ProprietarioControl
+{
+
+}
